954c5ba1d3cadc2d5477ad33d44b2eff11a83279
Juan David Castro <me@juandc.co>
2018-12-03 15:33:17 -0500

    Firebase user login (email and password)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-var form = document.forms['loginForm'];
-
-var firebaseConfig = {
+// Initial config
+const firebaseConfig = {
   apiKey: "AIzaSyA4zatcgkphW7EE3S6K2JN8CtjM1vAwQIk",
   authDomain: "platzi-firebase-simple-auth.firebaseapp.com",
   databaseURL: "https://platzi-firebase-simple-auth.firebaseio.com",
@@ -11,40 +10,62 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) return console.log('Habemus user 🎉');
+  return console.log('No habemus user 😭');
+});
+
+const form = document.forms['loginForm'];
 form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  var { email, password, isLoginOrSignup } = form;
+  const email = form['email'].value;
+  const password = form['password'].value;
+  const isLoginOrSignup = form['isLoginOrSignup'].value;
 
-  if (isLoginOrSignup.value === 'isLogin') {
-    loginUser({ email: email.value, password: password.value });
+  if (isLoginOrSignup === 'isLogin') {
+    loginUser({ email, password });
   } else {
-    createUser({ email: email.value, password: password.value });
+    createUser({ email, password });
   }
 })
 
+
 // Firebase defs
 function createUser({ email, password }) {
-  firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
-    alert('Se creo tu useer, bro :D')
-  }).catch(function (error) {
-    switch (error.code) {
-      case 'auth/email-already-in-use':
-        var soLogin = confirm(
-          'Ya te habias registrado con este email bro 😝, ¿quieres iniciar sesión ✨?'
+  console.log('Creating user ' + email);
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function (user) {
+      console.log('¡Creamos el user! Huepaje!');
+      // showPrivateInfo();
+    })
+    .catch(function (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('Ya existe el usuario');
+        const soLogin = confirm(
+          'Ya te habias registrado con este email, bro 😝. ¿Quieres iniciar sesión ✨?'
         );
-        !!soLogin ? loginUser({ email, password }) : alertTryAgain(error);
-      break;
-      default:
-        alertTryAgain(error);
-      break;
-    }
-  });
+        return !!soLogin ? loginUser({ email, password }) : alertTryAgain(error);;
+      }
+
+      return alertTryAgain(error);
+    });
 }
 
 function loginUser({ email, password }) {
-  console.log('Loging user ' + email)
+  console.log('Loging user ' + email);
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function (user) {
+      console.log('Credenciales correctas, brother');
+      // showPrivateInfo();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
+
 
 // General Utils
 function alertTryAgain(error) {
